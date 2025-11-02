2e28b5d4997b1b99ac15ee5723a948024e179d11
eddierose42 <eddie.rose@lis.ac.uk>
2025-11-02 17:56:35 +0000

    first draft finished

---
+++ app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from shiny import App, ui, render, reactive
+
+df = pd.read_csv("attendance_anonymised.csv")
+df.rename({"Long Description": "Module Name", "Postive Marks": "Attended", "Planned Start Date": "Date",}, axis=1, inplace=True)
+df["Date"] = pd.to_datetime(df["Date"])
+modules = list(np.unique(df["Module Name"]))
+
+gui = ui.page_fluid(
+    ui.h1("Average attendance over time"),
+    ui.input_select("dropdown", "Module name", choices=modules, selected=None),
+    ui.output_plot("plot")
+    )
+
+def server(input, output, session):
+    #@output
+    @render.plot(alt="line graph")  
+    def plot():
+        data = df[df["Module Name"] == input.dropdown()][["Date", "Attended"]].groupby("Date").mean()
+        fig, ax = plt.subplots()
+        ax.plot(data)
+        plt.xlabel("Date")
+        plt.ylabel("Avg. attendance")
+
+        return fig
+
+app = App(gui, server)
