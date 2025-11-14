5a6426319b9d4b5af87b5419c8fa64372838ff2f
eddierose42 <eddie.rose@lis.ac.uk>
2025-11-14 16:59:14 +0000

    Second draft

--- app.py
+++ app.py
@@ -3,27 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from shiny import App, ui, render, reactive
 
+# setup and clean data
 df = pd.read_csv("attendance_anonymised.csv")
 df.rename({"Long Description": "Module Name", "Postive Marks": "Attended", "Planned Start Date": "Date",}, axis=1, inplace=True)
 df["Date"] = pd.to_datetime(df["Date"])
 modules = list(np.unique(df["Module Name"]))
 
+# setup interface objects
 gui = ui.page_fluid(
     ui.h1("Average attendance over time"),
     ui.input_select("dropdown", "Module name", choices=modules, selected=None),
     ui.output_plot("plot")
     )
 
+# setup server and reactive plotting function
 def server(input, output, session):
-    #@output
-    @render.plot(alt="line graph")  
+    @render.plot(alt="line-graph")
     def plot():
+        # select correct module and take average for each date, then plot it
         data = df[df["Module Name"] == input.dropdown()][["Date", "Attended"]].groupby("Date").mean()
         fig, ax = plt.subplots()
         ax.plot(data)
         plt.xlabel("Date")
         plt.ylabel("Avg. attendance")
-
         return fig
 
 app = App(gui, server)
